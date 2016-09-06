e237bb9fb5b46b0e81df20bad9b8930e3316c37c
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-09-06 18:24:55 -0500

    ejercicio 3

---
+++ app.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function() {
+	var boton = document.getElementById("calcular");
+	boton.addEventListener("click", function() {
+		var segundos = parseInt(document.getElementById("segundos").value);
+		var numero = parseInt(document.getElementById("numero").value);
+
+		var resultado = document.getElementById("resultado");
+		setInterval(function(){ document.getElementById("resultado").innerHTML = numero++; }, segundos*1000);
+		document.getElementById("segundos").value = "";
+		document.getElementById("numero").value = "";
+	});
+});
