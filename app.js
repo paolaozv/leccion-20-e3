8c5ac61d928490d0c4b0848fef5b02b8508810f5
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2016-10-22 10:12:34 -0500

    modificación de indentación

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("calcular");
+	
 	boton.addEventListener("click", function() {
 		var segundos = parseInt(document.getElementById("segundos").value);
 		var numero = parseInt(document.getElementById("numero").value);
-
 		var resultado = document.getElementById("resultado");
-		setInterval(function(){ document.getElementById("resultado").innerHTML = numero++; }, segundos*1000);
+
+		setInterval(function(){ 
+			document.getElementById("resultado").innerHTML = numero++;
+		}, segundos*1000);
+
 		document.getElementById("segundos").value = "";
 		document.getElementById("numero").value = "";
 	});
